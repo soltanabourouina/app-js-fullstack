4e84b8e32a55d3000cfc61f44360efdaefd5fab8
soltana bourouina <bourouina.soltana@gmail.com>
2024-02-26 09:56:26 +0100

    refacto

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -4,15 +4,10 @@ const { setPosts, getPosts, editPost, deletePost, likePost, disLikePost } = requ
 const router = express.Router();
 
 router.get("/", getPosts);
-
 router.post("/", setPosts);
-
 router.put("/:id", editPost);
-
 router.delete("/:id", deletePost);
-
 router.patch ("/like-post/:id", likePost);
-
 router.patch ("/dislike-post/:id",disLikePost);
 
 module.exports = router;
